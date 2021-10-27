5e92e1defde2134f53916bc1e434f26864f55d18
akio0911 <akio0911@gmail.com>
2021-10-28 08:02:44 +0900

    feedback

--- Kadai8/RedViewController.swift
+++ Kadai8/RedViewController.swift
@@ -10,18 +10,18 @@ import UIKit
 class RedViewController: UIViewController {
     @IBOutlet private weak var numberLabel: UILabel!
     @IBOutlet private weak var slider: UISlider!
-    private weak var delegate = UIApplication.shared.delegate as? AppDelegate
+
+    // swiftlint:disable:next force_cast weak_delegate
+    private var appDelegate = UIApplication.shared.delegate as! AppDelegate
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        guard let slidervalue = delegate?.sharedSliderValue else { return }
-        slider.value = slidervalue
-        numberLabel.text = String(delegate!.sharedSliderValue)
+        slider.value = appDelegate.sharedValue
+        numberLabel.text = String(appDelegate.sharedValue)
     }
 
     @IBAction private func sliderAction(_ sender: Any) {
-        let sliderValue = slider.value
-        numberLabel.text = String(sliderValue)
-        delegate!.sharedSliderValue = sliderValue
+        numberLabel.text = String(slider.value)
+        appDelegate.sharedValue = slider.value
     }
 }
